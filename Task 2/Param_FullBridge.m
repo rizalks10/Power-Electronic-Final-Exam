472afb4c06d8eb9d761b7bccfbeea134bb099231
rizalks10 <119123320+rizalks10@users.noreply.github.com>
2025-12-10 13:40:51 +0700

    Add files via upload

---
+++ Task 2/Param_FullBridge.m
@@ -0,0 +1,22 @@
+% Single-phase full-bridge inverter parameters
+Vdc = 400;       % V    : DC voltage (input to the full-bridge)
+
+ma  = 1;         % —    : modulation index 
+
+D   = 0.5;       % —    : duty ratio for PWM 
+
+fsw = 10000;     % Hz   : switching frequency of the power switches (PWM carrier)
+
+fs  = 50;        % Hz   : output fundamental (grid) frequency
+
+ph  = 0;         % degree  : phase angle of the reference fundamental (degree)
+
+R   = 1;
+
+C1  = 1e-6;
+
+C2  = 1e-6;
+
+
+
+
